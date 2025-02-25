f034d940dbad15e920c57371db58614927f6f7ca
Stollin, Thomas <Thomas.Stollin@interhyp.de>
2025-02-25 15:45:36 +0100

    feat(Github-App)!: replace validation commit statuses with check-runs, update README.md
    BREAKING CHANGE: use check-runs instead of commit statuses and pull-request comments for yaml validation

--- internal/acorn/repository/github.go
+++ internal/acorn/repository/github.go
@@ -3,16 +3,28 @@ package repository
 import "context"
 
 type Github interface {
-	SetCommitStatusInProgress(ctx context.Context, repoPath, repoName, commitID, url string, statusKey string) error
-
-	SetCommitStatusSucceeded(ctx context.Context, repoPath, repoName, commitID, url string, statusKey string) error
-
-	SetCommitStatusFailed(ctx context.Context, repoPath, repoName, commitID, url string, statusKey string) error
-
-	CreatePullRequestComment(ctx context.Context, repoPath, repoName, pullRequestID, text string) error
-
-	GetChangedFilesOnPullRequest(ctx context.Context, repoPath, repoName, pullRequestID, toRef string) ([]File, string, error)
+	StartCheckRun(ctx context.Context, owner, repoName, checkName, sha string) (int64, error)
+	ConcludeCheckRun(ctx context.Context, owner, repoName, checkName string, checkRunId int64, conclusion CheckRunConclusion, details CheckRunDetails) error
+	GetChangedFilesForCommit(ctx context.Context, repoPath, repoName, sha string) ([]File, error)
 }
+
+type CheckRunConclusion string
+
+type CheckRunDetails struct {
+	Title    string
+	Summary  string
+	BodyText string
+}
+
+const (
+	CheckRunSuccess        CheckRunConclusion = "success"
+	CheckRunFailure        CheckRunConclusion = "failure"
+	CheckRunActionRequired CheckRunConclusion = "action_required"
+	CheckRunTimedOut       CheckRunConclusion = "timed_out"
+	CheckRunCancelled      CheckRunConclusion = "cancelled"
+	CheckRunNeutral        CheckRunConclusion = "neutral"
+	CheckRunSkipped        CheckRunConclusion = "skipped"
+)
 
 type CommitBuildStatus string
 
